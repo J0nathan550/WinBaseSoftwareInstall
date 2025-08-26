63fdba3eb42707ab8de8315603339a6ecbfbeaff
J0nathan550 <ihudokormov@gmail.com>
2025-08-26 14:41:19 +0200

    Add project files.

---
+++ WinBaseSoftwareInstall/App.xaml.cs
@@ -0,0 +1,8 @@
+﻿using System.Windows;
+
+namespace WinBaseSoftwareInstall
+{
+    public partial class App : Application
+    {
+    }
+}
---
+++ WinBaseSoftwareInstall/Views/MainWindowView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace WinBaseSoftwareInstall.Views
+{
+    public partial class MainWindowView : Window
+    {
+        public MainWindowView()
+        {
+            InitializeComponent();
+        }
+    }
+}
